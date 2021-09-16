f455457b01b7b024e566dce2b068cd87a51baee7
ianz <yiyang.zhong@outlook.fr>
2021-09-16 12:42:53 +0200

    feat: implement createPost

---
+++ create.js
@@ -0,0 +1,28 @@
+const fetch = require("node-fetch");
+const api = require("./api");
+
+async function postRequest(url, data) {
+    const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+        },
+    });
+    return await response.json();
+}
+
+async function createPost(title, body, userId) {
+    const url = api.RESOURCES.posts;
+    let responseData = await postRequest(url, {
+        title: title,
+        body: body,
+        userId: userId,
+    });
+    console.log(responseData);
+    return responseData;
+}
+
+module.exports = {
+    createPost: createPost,
+}
