3821ba0802083c99599ff28a23d9b05a50ac352d
ianz <yiyang.zhong@outlook.fr>
2021-09-16 14:09:47 +0200

    test: add test for post delete operation

---
+++ test/delete.test.js
@@ -0,0 +1,13 @@
+const expect = require("chai").expect;
+const del = require("../delete");
+
+describe("Delete operation", function () {
+
+    context("Posts resources", function () {
+        it("should return 200", async function () {
+            const postId = 42;
+            const response = await del.deletePostWithPostId(postId);
+            expect(response).to.have.property("status", 200);
+        });
+    });
+});
