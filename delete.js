47fc71c01215d3b3258bf1a7b8548a9728f1eb2a
ianz <yiyang.zhong@outlook.fr>
2021-09-16 14:10:06 +0200

    feat: implement deletePostWithPostId

---
+++ delete.js
@@ -0,0 +1,23 @@
+const fetch = require("node-fetch");
+const api = require("./api");
+
+async function deleteRequest(url) {
+    return await fetch(url, {
+        method: "DELETE",
+    });
+}
+
+async function deletePostWithPostId(postId) {
+    const url = api.RESOURCES.posts + "/" + postId;
+    const response = await deleteRequest(url);
+    if (response.status === 200) {
+        console.log("Post " + postId+ " has been deleted successfully.");
+    } else {
+        console.log("Oops! Something went wrong...");
+    }
+    return response;
+}
+
+module.exports = {
+    deletePostWithPostId: deletePostWithPostId,
+}
