07774ff3c01241bf7f653ccb8a0f10de6831273b
ianz <yiyang.zhong@outlook.fr>
2021-09-17 14:11:44 +0200

    feat: implement patchRequest

--- update.js
+++ update.js
@@ -35,6 +35,37 @@ async function updatePostWithPostId(postId, title, body, userId) {
     return responseData;
 }
 
+async function patchTitleOfPostWithPostId(postId, title) {
+    const url = api.RESOURCES.posts + "/" + postId;
+    let responseData = await patchRequest(url, {
+        title: title,
+    });
+    console.log(responseData);
+    return responseData;
+}
+
+async function patchBodyOfPostWithPostId(postId, body) {
+
+    const url = api.RESOURCES.posts + "/" + postId;
+    let responseData = await patchRequest(url, {
+        body: body,
+    });
+    console.log(responseData);
+    return responseData;
+}
+
+async function patchUserIdOfPostWithPostId(postId, userId) {
+    const url = api.RESOURCES.posts + "/" + postId;
+    let responseData = await patchRequest(url, {
+        userId: userId,
+    });
+    console.log(responseData);
+    return responseData;
+}
+
 module.exports = {
     updatePostWithPostId: updatePostWithPostId,
+    patchTitleOfPostWithPostId: patchTitleOfPostWithPostId,
+    patchBodyOfPostWithPostId: patchBodyOfPostWithPostId,
+    patchUserIdOfPostWithPostId: patchUserIdOfPostWithPostId,
 }
